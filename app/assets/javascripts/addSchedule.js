5e202e8951907ddd0205e4420c743c1e88088710
ken <b98705022@ntu.edu.tw>
2012-05-31 01:04:13 +0800

    ken 0531 0104

--- app/assets/javascripts/addSchedule.js
+++ app/assets/javascripts/addSchedule.js
@@ -1,9 +1,12 @@
 function addSchedule(){ 
     var newSpotName = $('#infoWindow ul li a').html();
-    var newSpotLat = $('#infoWindow ul li p').html();
-    var newSpotLon = $('#infoWindow ul li p').eq(1).html();
-    var newSpotAddress = $('#infoWindow ul li p').eq(2).html();
-    var newSpotText = $('#infoWindow ul li p').eq(3).html();
+    var newSpotID = $('#infoWindow ul li p').html();
+    var newSpotLat = $('#infoWindow ul li p').eq(1).html();
+    var newSpotLon = $('#infoWindow ul li p').eq(2).html();
+    var newSpotAddress = $('#infoWindow ul li p').eq(3).html();
+    var newSpotText = $('#infoWindow ul li p').eq(4).html(); 
     //if 已經在schedule則不append 可以用img判斷
-    $('#mySchedule').append('<li class="block spotinfo" id="1" name="'+newSpotName+'" zoom="1.0" lat="'+newSpotLat+'" lon="'+newSpotLon+'" address="'+newSpotAddress+'" info="'+newSpotText+'" style="z-index: 3; "><a href="javascript:lightbox(\''+newSpotName+'\')">'+newSpotName+'</a><img src="/img/1" height="60%" width="90%"><div class="travel_time_space"><div class="travel_time_content">4hr</div></div></li>')
+    //if ()
+    $('#mySchedule').append('<li class="block spotinfo" id="'+newSpotID+'" name="'+newSpotName+'" zoom="1.0" lat="'+newSpotLat+'" lon="'+newSpotLon+'" address="'+newSpotAddress+'" info="'+newSpotText+'" in = "1" style="z-index: 3; "><a href="javascript:lightbox(\''+newSpotName+'\')">'+newSpotName+'</a><img src="/img/1" height="60%" width="90%"><div class="travel_time_space"><div class="travel_time_content">4hr</div></div></li>')
     //this.setIcon("http://maps.google.com/mapfiles/markerB.png");//加入schedule 後要改變圖示
+}
