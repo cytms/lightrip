c22ce5bdcbfce527356e87c86e9ada87f19ea169
ken <b98705022@ntu.edu.tw>
2012-05-31 01:08:32 +0800

    ken 0531 0108

--- app/assets/javascripts/info.js
+++ app/assets/javascripts/info.js
@@ -4,6 +4,6 @@ function information(id, spotName, lat, lon, text, address){
       infowindow.setContent("<div id='infoWindow'><ul><li><h1><a href=javascript:lightbox(\'"+spotName+"\')>"+spotName+"</h1></a><p hidden='true'>"+id+"</p><p hidden='true'>"+lat+"</p><p hidden='true'>"+lon+"</p><p hidden='true'>"+address+"</p><p>"+text+"</p><img src='assets/add.png' onclick='addSchedule();'/></li></ul></div>");
       infowindow.open(map, this);
       });//如果用mousevoer時會產生事件衝突?可以用自訂overlay解決
-    //google.maps.event.addListener(marker, 'mouseout', function () {
-      //infowindow.close();});   
+    google.maps.event.addListener(map, 'click', function () {
+      infowindow.close();});   
 }
