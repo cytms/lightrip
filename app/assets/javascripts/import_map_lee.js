5d51a690565d745a360cb0116c1ce6940483458e
cytms <cytmos@gmail.com>
2012-05-30 17:29:42 +0800

    Cytms + ckpeter 0530

---
+++ app/assets/javascripts/import_map_lee.js
@@ -0,0 +1,230 @@
+/**import_map.js**/
+/*$(function(){ 
+  $('#myMap').tinyMap({ 
+      center: {x: '25.03369510279853', y: '121.56480431556702'}, 
+      zoom: 16 
+  }); 
+
+  $('#myMap').tinyMap({ 
+      marker: [ 
+          {addr: '25.037467, 121.564077', text: '台北政府'}, 
+          {addr: '25.100295, 121.549494', text: '國立故宮博物院'} 
+      ] 
+  }); 
+});*/
+
+google.load("jquery", "1.3");
+google.load("jqueryui", "1.7");
+
+google.setOnLoadCallback(function() {
+	$('#main').css('-moz-user-select', 'none'); //禁止fx選取文字
+	$('#main').get(0).onselectstart = function(){return false;}; //禁止IE選取文字
+	var temp_counter = 0;
+	// 所有行程
+	$(function() {
+		$( "#mapFrame, #mySchedule" ).sortable({
+			connectWith: ".connectedSortable",
+			cancel: ".ui-state-disabled",
+			change: function(event, ui) {
+			//alert("activate!!")
+			if(temp_counter == 0){
+				console.log("next html:" + ui.item.next().html());
+				if (ui.item.prev().attr('class') == "trans ui-state-disabled" ) {
+				ui.item.prev().remove();
+				};
+
+				if (ui.item.next().attr('class') == "trans ui-state-disabled" ) {
+					ui.item.next().remove();
+				};
+				$('<li class="trans ui-state-disabled">middle</li>').insertAfter(ui.item);
+			};
+			temp_counter = 1;
+
+			},
+
+			stop: function(event, ui) {
+			//alert("stop!!");
+			temp_counter = 0;	
+			//alert('class:' + ui.item.prev().attr('class'));
+			if (ui.item.prev().attr('class') == "trans ui-state-disabled" ) {
+				ui.item.prev().remove();
+			};
+
+			if (ui.item.next().attr('class') == "trans ui-state-disabled" ) {
+				ui.item.next().remove();
+			};
+			
+			$('<li class="trans ui-state-disabled">after</li>').insertAfter(ui.item);
+			$('<li class="trans ui-state-disabled">before</li>').insertBefore(ui.item);
+			//ui.item.append('<li class="trans ui-state-disabled" id="trans_' + data[i]['id'] + '">');
+			//ui.item.prepend('<li class="trans ui-state-disabled" id="trans_' + data[i]['id'] + '">');
+			//ui.item.append('<li class="trans ui-state-disabled" id="trans_' + data[i]['id'] + '">');
+			if ( $('ul#mySchedule li:last').attr('class') == "trans ui-state-disabled" ) {
+				$('ul#mySchedule li:last').remove();
+			};
+			
+			if ( $('ul#mySchedule li:first').attr('class') == "trans ui-state-disabled" ) {
+				$('ul#mySchedule li:first').remove();
+			};
+
+			}
+		}).disableSelection();
+		
+		/*$(".block").resizable({
+			maxHeight: 300,
+			maxWidth: 150,
+			minHeight: 40,
+			minWidth: 150,
+		});*/
+
+		/*$(".active").click(function() {
+			//$("form").submit();
+			$(".menu").hide();//嵌入body裡面會不會比較好
+			$("#scheduleFrame").show();
+			$("#buttonFrame").show();
+			$(".firstpage").hide();
+		});*/
+		$("#home").click(function() {
+			$(".menu").show();
+			$(".firstpage").show();
+			$("#scheduleFrame").hide();
+			//嵌入body裡面會不會比較好
+		});
+
+		//$("#drag").draggable();
+	});
+});
+
+//lightbox
+function lightbox(content) {
+	
+	$("body").append("<div class='bg'></div>");
+	$("body").append("<div class='lightbox'><div class='framebox'>" + content + "</div></div>");
+	$(".lightbox").fadeIn(1000);
+	$(".framebox").fadeIn(1000);
+	$(".bg").click(function() {
+		$(".lightbox").remove();//處裡消掉的東西
+		$(".bg").remove();//嵌入body裡面會不會比較好
+		
+	});
+	//if ($(".lightbox").attr("style:display") == "none"){
+			//$(".lightbox").remove();}
+}
+
+	$(function() {
+		$( "#slider" ).slider({
+			orientation: "vertical",
+			value:50,
+			min: 0,
+			max: 100,
+			step: 20,
+			slide: function( event, ui ) {
+				$( "#amount" ).val( ui.value );
+			}
+		});
+		$( "#amount" ).val( $( "#slider" ).slider( "value" ) );
+		
+		$( "#slider2" ).slider({
+			orientation: "vertical",
+			value:50,
+			min: 0,
+			max: 100,
+			step: 20,
+			slide: function( event, ui ) {
+				$( "#amount2" ).val( ui.value );
+			}
+		});
+		$( "#amount2" ).val( $( "#slider2" ).slider( "value2" ) );
+		
+		$( "#slider3" ).slider({
+			orientation: "vertical",
+			value:50,
+			min: 0,
+			max: 100,
+			step: 20,
+			slide: function( event, ui ) {
+				$( "#amount3" ).val( ui.value );
+			}
+		});
+		$( "#amount3" ).val( $( "#slider3" ).slider( "value3" ) );
+		$( "#slider4" ).slider({
+			orientation: "vertical",
+			value:50,
+			min: 0,
+			max: 100,
+			step: 20,
+			slide: function( event, ui ) {
+				$( "#amount4" ).val( ui.value );
+			}
+		});
+		$( "#amount4" ).val( $( "#slider4" ).slider( "value4" ) );
+		$( "#slider5" ).slider({
+			orientation: "vertical",
+			value:50,
+			min: 0,
+			max: 100,
+			step: 20,
+			slide: function( event, ui ) {
+				$( "#amount5" ).val( ui.value );
+			}
+		});
+		$( "#amount5" ).val( $( "#slider5" ).slider( "value5" ) );
+	});
+
+(function($){
+	$(document).ready(function(){
+		$('.active').click(function(){
+			$(".firstpage").hide();
+			$(".menu").hide();
+			$("#scheduleFrame").show();
+			$("#buttonFrame").show();
+			//alert("Handler for .click() called.");
+			$.ajax({
+				type: 'GET',
+				data: { attr1: $( "#amount" ).val(),
+				 attr2: $( "#amount2" ).val(),
+				 attr3: $( "#amount3" ).val(),
+				 attr4: $( "#amount4" ).val(),
+				 attr5: $( "#amount5" ).val()
+				},
+				//$( "#amount" ).html(),
+				url: "http://localhost:3000/step3",
+				//url: "http://lightrip-cytms.herokuapp.com/step3",
+				datatype: 'json',
+				success: function(data, textSatus){
+					//alert("ajax success");
+					//$(".firstpage").hide();//處裡消掉的東西
+					//$(".menu").hide();//嵌入body裡面會不會比較好
+					//$("#scheduleFrame").show();
+					//$("#buttonFrame").show();
+					//$("li.block:even").append()
+					//$('li.block:odd').append("<a href=javascript:lightbox('hahaha')>"+data['test']+"</a>");
+					//$('ul#mySchedule').append("<p><div onclick='calculateDistances();'>Calculate distances_driving(default) 打開console log</div></p>");
+				
+					for ( var i = 0; i < data.length ; i++) {
+						$('ul#mySchedule').append('<li class="block spotinfo" id="' + data[i]['id'] + '" name="' + data[i]['name'] + '" zoom="' + data[i]['zoom'] + '" lat="' + data[i]['lat'] + '" lon="' + data[i]['lon'] + '" address="' + data[i]['address'] +'" ><a href=javascript:lightbox("' + data[i]['address'] + '")>' + data[i]['name'] + '</a></li>');
+						
+
+						if( i != (data.length - 1)){
+							var origin1 = data[i]['address'];
+							var destinationA = data[(i+1)]['address'];
+							var traffic_id = "";
+							traffic_id = "traffic_" + origin1;
+							traffic_id += "_";
+							traffic_id += destinationA;
+							return_value = 0;
+								//$('ul#mySchedule').append('<li class="trans ui-state-disabled" id="trans_' + data[i]['id'] + '"><div id="outputDiv_'+origin1+'_'+destinationA+'">default</div></li>');
+								$('ul#mySchedule').append('<li class="trans ui-state-disabled" id="' + traffic_id + '"></li>');
+							calculateDistances_walking(origin1,destinationA,traffic_id);
+							console.log("traffic_id: " + traffic_id);
+
+						}
+						$('li#' + data[i]['id'] ).append('<img src="/img/' + data[i]['id'] + '" height="60%" width="90%"><div class="travel_time_space"><div class="travel_time_content">4hr</div></div>');
+					}
+
+				}
+			}
+			);
+		});
+	});
+})(jQuery);
---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,78 @@
+class HomeController < ApplicationController
+  include RestGraph::RailsUtil
+  require 'rest-graph'
+  before_filter :login_facebook, :only => [:login]
+  before_filter :load_facebook, :except => [:login]
+  before_filter :filter_setup_rest_graph
+  before_filter :filter_cache, :only => [:cache]
+
+  def index
+    @access_token = rest_graph.access_token
+
+    if @access_token
+      @me = rest_graph.get('/me')
+    end
+
+    @spots=Spot.all
+  end
+
+  def login
+    redirect_to home_path
+  end
+
+  def logout
+    reset_session
+    redirect_to home_path
+  end
+
+  def attr
+    respond_to do |format|
+      format.html # show.html.erb
+     # format.json { render json: @spot }
+    end
+  end
+
+  def smart
+    respond_to do |format|
+      format.html # show.html.erb
+     # format.json { render json: @spot }
+    end
+  end
+
+  def step3
+    # we will use params in the future  EX. params['attr1']
+
+    #respond_to do |format|
+    #    format.html # show.html.erb
+    #    format.json { render json: @spot }
+    #  end
+    #end
+    
+    @spots = Spot.all
+    hash = {:test => "YOYO"}
+    render json: @spots
+  end
+
+private
+  
+  def load_facebook
+    rest_graph_setup(:write_session => true)
+  end
+
+  def login_facebook
+    rest_graph_setup(:auto_authorize         => true,
+                     :auto_authorize_scope   => 'publish_checkins',
+                     :ensure_authorized      => true,
+                     :write_session          => true)
+  end
+
+  def filter_setup_rest_graph
+    rest_graph_setup(:auto_authorize => true,
+                      :auto_authorize_scope   => 'read_stream')
+  end
+
+  def filter_cache
+    rest_graph_setup(:cache => Rails.cache)
+  end
+
+end
