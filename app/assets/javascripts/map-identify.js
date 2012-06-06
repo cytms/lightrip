1641694be846bbacf12197a4a214edb675aa21da
cytms <cytmos@gmail.com>
2012-06-06 17:58:48 +0800

    adjust

--- app/assets/javascripts/map-identify.js
+++ app/assets/javascripts/map-identify.js
@@ -4,7 +4,7 @@ function spanid(CityName,point1,point2){
         
 }
 function spanidrestore(CityName){
-        document.getElementById('chosenSpots').innerHTML = CityName;
+        document.getElementById('chosenSpots').innerHTML = '<p style="font-size:36px; line-height:0px; text-align:right;">' + CityName + '</p>';
         /*if ( !$("area[title='" + CityName + "']").attr('class') ){
           $("area[title='" + CityName + "']").addClass('selected');
           document.getElementById('chosenSpots').innerHTML += CityName + " ";
