756551e953db9c0a62e83769378044be0954fb6d
cytms <cytmos@gmail.com>
2012-06-07 05:16:19 +0800

    wowowo

--- app/assets/javascripts/postSchedule.js
+++ app/assets/javascripts/postSchedule.js
@@ -1,18 +1,16 @@
 function postSchedule(){
       if ($('#userid').html()!=null){
         save();
-      	$("body").append("<div class='bg'></div>");
-        alert($('#mySchedule img').first().attr('src'));
-		$("body").append("<div class='postForm'><h2 class = 'header'>"+$('#schedule_name').html()+"</h2><img src='"+$('#mySchedule img').first().attr('src')+"'/><p>為您的行程加點註解</p><textarea class='description' row='2' cols ='25'></textarea><input class ='postfb' type='button' value='Post the schedule to facebook' onclick=''/></div>");
+      	//$("body").append("<div class='bg'></div>");
+        //alert($('#mySchedule img').first().attr('src'));
+		lightbox("<h2>" + $('#schedule_name').html() + "</h2><img src='" + $('#mySchedule img').first().attr('src') + "'/><p>為您的行程加點註解</p><textarea class='description' row='10' cols ='25'></textarea><p><input id ='postfb' type='button' class='clickme' value='Post the schedule to facebook'/></p>");
 //如果沒有行程會出錯
 
 		//$('.fbfriend2').append('<p>'+friendname+'</p>');
-		$(".postfb").click(function() {
+		$("#postfb").click(function() {
     var title = $('.header').html();
     var description = $('.description').val();
-		var src = $('.postForm img').attr('src');
-		$(".postForm").remove();//處裡消掉的東西
-		$(".bg").remove();//嵌入body裡面會不會比較好
+		var src = $('.framebox img').attr('src');
 		post(title, description, src);
 	});
 }
@@ -24,6 +22,7 @@ function postSchedule(){
       $("#menu").show();
     }
 }
+
 function post(title, description, src){
 	//alert(title);
   //alert(description);
